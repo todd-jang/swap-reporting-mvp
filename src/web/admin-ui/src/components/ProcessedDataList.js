625701735dd435ab55a718d99a921a4e7d9269ed
GiHoon Jang <stb.jang@gmail.com>
2025-05-05 22:32:26 +0900

    Create ProcessedDataList.js

---
+++ src/web/admin-ui/src/components/ProcessedDataList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// TODO: Import Redux slice and fetch logic for processed data
+
+function ProcessedDataList() {
+  // TODO: Fetch processed data using Redux thunk and display in a table
+  return (
+    <div className="card">
+      <div className="card-header">Processed Data</div>
+      <p>This page will list processed swap data records.</p>
+      {/* TODO: Implement fetching, filtering, pagination, and table display */}
+    </div>
+  );
+}
+
+export default ProcessedDataList;
