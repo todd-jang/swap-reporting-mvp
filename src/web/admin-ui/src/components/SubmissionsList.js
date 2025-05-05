f7520bbd22daf175ab46a63fd582adba41468b8b
GiHoon Jang <stb.jang@gmail.com>
2025-05-05 22:33:48 +0900

    Create SubmissionsList.js

---
+++ src/web/admin-ui/src/components/SubmissionsList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// TODO: Import Redux slice and fetch logic for submissions
+
+function SubmissionsList() {
+   // TODO: Fetch submission history using Redux thunk and display in a table
+  return (
+    <div className="card">
+      <div className="card-header">Submission History</div>
+      <p>This page will list report submission attempts.</p>
+       {/* TODO: Implement fetching, filtering, pagination, and table display */}
+       {/* TODO: Add links to view submission details */}
+    </div>
+  );
+}
+
+export default SubmissionsList;
