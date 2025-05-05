8b919719f03b797d44fb5837ac5061c7052b25f2
GiHoon Jang <stb.jang@gmail.com>
2025-05-05 22:25:57 +0900

    Create index.js

---
+++ src/web/admin-ui/src/index.js
@@ -0,0 +1,13 @@
+// src/admin-ui/src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css'; // Global styles if any
+import App from './App';
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
