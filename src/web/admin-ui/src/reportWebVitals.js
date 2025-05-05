16287c6034d835f3aedcc46a3096e01e51341514
GiHoon Jang <stb.jang@gmail.com>
2025-05-05 22:54:11 +0900

    Create reportWebVitals.js

---
+++ src/web/admin-ui/src/reportWebVitals.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// Function to report web vitals - standard Create React App file
+const reportWebVitals = onPerfEntry => {
+  if (onPerfEntry && onPerfEntry instanceof Function) {
+    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+      getCLS(onPerfEntry);
+      getFID(onPerfEntry);
+      getFCP(onPerfEntry);
+      getLCP(onPerfEntry);
+      getTTFB(onPerfEntry);
+    });
+  }
+};
+
+export default reportWebVitals;
