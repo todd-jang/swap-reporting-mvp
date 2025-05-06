7866545e116a281acc8a6abacde268faef3522a0
GiHoon Jang <stb.jang@gmail.com>
2025-05-06 13:48:55 +0900

    Update urls.py

--- src/web/project_cache-validate&test/urls.py
+++ src/web/project_cache-validate&test/urls.py
@@ -1,1 +1,10 @@
+# my_django_project/urls.py
 
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('my-app/', include('my_app.urls')), # my_app의 URL 포함
+    # ... 다른 URL 패턴 ...
+]
